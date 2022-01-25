50666819ce3922e40b22bbe1c2eb94795e3324bd
Julian <julianprice8@gmail.com>
2022-01-25 18:30:09 -0500

    Added staff list router link on main page.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
+    <router-link to="/staff">Staff List</router-link>
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
