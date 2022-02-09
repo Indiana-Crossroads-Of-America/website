868e7f66009d78ef895cd10b2f7dcc886d6673f2
Julian <julianprice8@gmail.com>
2022-02-08 19:47:31 -0500

    Added Photographs view with a small paragraph. Still need to link it up.

---
+++ src/views/Photographs.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="photographs">
+    <p>
+      These are but a few excellent photographs posted by members of our Discord
+      server and shared with the community.
+    </p>
+  </div>
+</template>
