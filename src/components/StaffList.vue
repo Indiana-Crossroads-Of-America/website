bec3890234c313ff54fa375d0dda636eb4f8c059
Ross L. Mersch <me@rossmers.ch>
2022-02-16 14:26:24 -0500

    Adding some styling.
    Added some layout changes, spacing and periods.

--- src/components/StaffList.vue
+++ src/components/StaffList.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="stafflist">
-    <h1>The team at I:COA</h1>
+    <h1>Operations at I: COA</h1>
+    <hr />
     <h1>Leadership <br /><font size="2">The bosses of the bosses.</font></h1>
     <label
-      ><b>JulesBeHere:</b>
+      ><b>JulesBeHere: </b>
       <font size="2">Project Founder & Head Administrator</font></label
     ><br />
     <h1>
+      <hr />
       Server Staff <br /><font size="2"
         >Those whom enforce our server policies.</font
       >
@@ -16,14 +18,16 @@
     <label><b>PeePee Actual:</b> <font size="2">Server Moderator</font></label
     ><br />
     <h1>
+      <hr />
       DevOps Team <br /><font size="2"
         >Those whom contribute significantly to the develop of I:COA.
       </font>
     </h1>
     <label
-      ><b>Ross:</b>
+      ><b>Ross: </b>
       <font size="2">DevOPS Coordinator & Network Manager</font></label
     >
+    <hr />
   </div>
 </template>
 <script>
