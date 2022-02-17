61d1606f7dc27a2176a7d2bfddd4db43554c9493
Julian <julianprice8@gmail.com>
2022-02-17 00:39:50 -0500

    Re-organized code.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="home">
-<h1>Welcome to Indiana & Our Community.</h1>
-<p>We're a community dedicated to equality, fun, wholness, and fun.</p>
-<p>We enjoy bringing others together for <a href="/events">events</a></p>
-<p style="border:1px; border-style:solid; border-color:#FF0000; padding: 3em;"></p>
+    <h1>Welcome to Indiana & Our Community.</h1>
+    <p>We're a community dedicated to equality, fun, wholness, and fun.</p>
+    <p>We enjoy bringing others together for <a href="/events">events</a></p>
+    <p
+      style="
+        border: 1px;
+        border-style: solid;
+        border-color: #ff0000;
+        padding: 3em;
+      "
+    ></p>
   </div>
 </template>
 
