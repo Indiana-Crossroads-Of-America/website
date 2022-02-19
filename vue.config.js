a5c3d643d69ca3327ad9d324c98d81f6b2493524
Julian <julianprice8@gmail.com>
2022-02-19 00:13:21 -0500

    Added space on comment.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   devServer: {
-    disableHostCheck: true, //Temporary bypass for development, will need a valid header before we can launch.
-
+    disableHostCheck: true, // Temporary bypass for development, will need a valid header before we can launch.
   },
 };
