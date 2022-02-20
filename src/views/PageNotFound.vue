62a9267cb367df95bf9f9574dbc7fb7a043bcb65
Ross L. Mersch <ross@indianacrossroadsofamerica.com>
2022-02-19 23:05:34 -0500

    404 handler

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>404 Page my dawg</h1>
+  <h1>(404) Sorry, that content wasn't found!</h1>
+  <p>If you believe this is a mistake, check your URL.</p>
+  <i>Here's a picture of a cute dog instead! (not interested? <a href="/">go home.</a>)</i> <br>
+  <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg" width="1200" height="600">
 </template>
 
 <script>
