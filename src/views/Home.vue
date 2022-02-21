7de0b9f8dbe6d835cde271a2fd45536090b7b126
Julian <julianprice8@gmail.com>
2022-02-20 21:02:17 -0500

    Added Discord tutorial to homepage.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,15 @@
       Join our community: <a href="https://discord.gg/5AUD5g8kgR">here</a>.
       <i>(You're Always welcomed to I:COA)</i>
     </p>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/rnYGrq95ezA"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
   </div>
 </template>
 
