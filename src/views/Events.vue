e29a516922896e15bc332f058f1a8ff0e028d6b7
Julian <julianprice8@gmail.com>
2022-02-21 17:33:33 -0500

    Added new event. Created event class. Increased font size.

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,38 +1,65 @@
 <template>
   <div class="alert">
-    <marquee><strong>Notice:</strong> Due to COVID-19, face masks <u>may be</u> required.
-    Stay home if you've come in contact with COVID-19 AND/OR are experiencing
-    symptoms of COVID-19.</marquee>
+    <marquee
+      ><strong>Notice:</strong> Due to COVID-19, face masks
+      <u>may be</u> required. Stay home if you've come in contact with COVID-19
+      AND/OR are experiencing symptoms of COVID-19.</marquee
+    >
   </div>
   <h1>Community Events</h1>
   <pre>Events are localized by County.</pre>
-<hr class="coa">
+  <hr class="coa" />
+  <div id="event" class="event">
+    <p
+      style="
+        border: 1px;
+        border-style: solid;
+        border-color: #ff0000;
+        padding: 3em;
+      "
+    >
+      <br />
+      <b
+        >EVENT 1: <br />
+        Name: NineStar Connect Gaming LAN (Hancock County/Greenfield, IN)</b
+      >
+      <br />
+      Location: 2243 E Main St, Greenfield, IN 46140 Date: 2/25/2022 <br />
+      Time: 6:00PM - 9PM Eastern Time
+      <br />
+      Event Info: GAMING LAN, 9* <i>sometimes</i> provides up to 10 GAMING
+      Computers for players to game on. They also have a Nintendo Switch and
+      host a Smash Bro's Competition.
+      <br />
+      <br />
+      <i
+        >This event is set to begin at 6PM-EST, and end at 9PM-EST. This may
+        change due to weather, availability, or other factors.
+      </i>
+    </p>
+  </div>
 
-  <p
-    style="
-      border: 1px;
-      border-style: solid;
-      border-color: #ff0000;
-      padding: 3em;
-    "
-  >
-    <br />
-    <b
-      >EVENT 1: <br />
-      Name: NineStar Connect Gaming LAN (Hancock County/Greenfield, IN)</b
+  <div id="event" class="event">
+    <p
+      style="
+        border: 1px;
+        border-style: solid;
+        border-color: #ff0000;
+        padding: 3em;
+      "
     >
-    <br />
-    Location: 2243 E Main St, Greenfield, IN 46140 Date: 2/25/2022 <br />
-    Time: 6:00PM - 9PM Eastern Time
-    <br>
-    Event Info: GAMING LAN, 9* <i>sometimes</i> provides up to 10 GAMING Computers for players to game on. They also have a Nintendo Switch and host a Smash Bro's Competition.
-    <br />
-    <br />
-    <i
-      >This event is set to begin at 6PM-EST, and end at 9PM-EST. This may change due to weather, avaliability, or other concluding factors.
-      </i
-    >
-  </p>
+      <br />
+      <b>Movie Night #3</b><br />
+      <br />
+      Location: Discord Lounge Voice Chat <br />
+      Date: 2/25/2022 <br />
+      Time: <strong>8PM</strong> Eastern Time
+      <br />
+      Event Info: We're watching Armaggedon!
+      <br />
+      <br />
+    </p>
+  </div>
 
   <!-- <p>Sorry, there are no events currently. Check back later.</p>
   If you'd like to share an event, contact
@@ -71,6 +98,10 @@
 
 .closebtn:hover {
   color: black;
+}
+
+.event {
+  font-size: 20px;
 }
 </style>
 
