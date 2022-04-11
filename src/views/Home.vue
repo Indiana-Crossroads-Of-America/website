02f79f5f527152ab42d12a39ae74052ad58216cf
Ross L. Mersch <ross@indianacrossroadsofamerica.com>
2022-04-11 13:39:05 -0400

    Fixes some formatting errors, <p> tags weren't closed properly.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,11 @@
       We're a community dedicated to bring people together regardless of
       differences,.
     </p>
-    <p>
-      Join our community: <a href="https://discord.gg/5AUD5g8kgR">here</a>.
+
+    <p> Join our community: <a href="https://discord.gg/5AUD5g8kgR">here</a>. </p>
       <i>(You're Always welcomed to I:COA)</i>
       <pre><b>We're Hiring (volunteers)</b>, see our current <router-link to="/opportunities">opportunities</router-link>.</pre>
-    </p>
-<br> 
+  <br>
     <video width="800" height="650" controls>
   <source src="https://icoa.s3.us-west-2.amazonaws.com/videos/disc_guide.mp4" type="video/mp4"></video>
   </div>
--- src/views/Opportunities.vue
+++ src/views/Opportunities.vue
@@ -2,7 +2,7 @@
     <p><b>We're Accepting Volunteers!</b></p>
     <p>We're seeking CSS Developers, HTML Developers & VUEJS Developers. 
         <br />Our current team <u>Volunteers</u> our time to develop the site.</p>
-<br>
+  <br>
   <p>Do you have what it takes? You can reach out via Discord (JulesBeHere#9371) or Email the <a href="mailto:acquisition@indianacoa.com">Talent acquisition</a> team.</p>
 
 </template>
