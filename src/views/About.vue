2e8ef68137a91437de87fe1c84fa9d335d545e5b
Julian <julianprice8@gmail.com>
2022-04-26 17:06:39 -0400

    Updated About page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="about">
-    <h1>
-     What is Indiana: Crossroads of America?
-    </h1>
-    <p>We're a server dedicated to bringing everyone together.  We do this regardless of differences, sexual orientation, religion or other factors.</p>
-    <h1>Why was this created?</h1>
-    <p>I:COA was created with the mindset that everyone is welcome. We are tired of big servers shunning people out, which is why we created an all-inclusive welcoming server.</p>
+    <h1>What is Indiana: Crossroads of America?</h1>
+    <p>
+      Indiana: Crossroads of America is primarily a Discord community that's
+      dedicated to bringing the people of Indiana together. We do this
+      regardless of differences, sexual orientation, religion or other factors.
+    </p>
+    <h1>Why did we create this community?</h1>
+    <p>
+      The Indiana: Crossroads of America community was founded when its creator
+      realized such a community could help bring people from the state together,
+      especially in this relatively new digital era.
+    </p>
   </div>
 </template>
